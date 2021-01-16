918fdb86fc4df322276fa63e9075ec54b5a42f59
JoshuaYe <joshua.ye@live.com>
2021-01-16 13:58:08 -0500

    created front page and added modal form

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import Header from "./Header";
+import LandingPage from "./LandingPage";
+import JoinUs from "./JoinUs";
+import history from "../history";
 
 const App = () => {
   return (
-    <div>
-      <h1>Dojo</h1>
-      <button>Learn more here</button>
+    <div className="ui container">
+      <Router history={history}>
+        <Header />
+        <div>
+          <Switch>
+            <Route path="/" exact component={LandingPage} />
+            <Route path="/join" exact component={JoinUs} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 };
