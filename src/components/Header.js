508f93915265acc0845dd02753454834418451e7
JoshuaYe <joshua.ye@live.com>
2021-01-16 15:13:41 -0500

    fixed heading styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div
       className="ui secondary pointing menu"
       style={{ backgroundColor: "#947F4B" }}
-    >
-      <Link to="/" className="item">
-        
-      </Link>
-    </div>
+    ></div>
   );
 };
 
--- src/components/VideoCall.js
+++ src/components/VideoCall.js
@@ -1,16 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const VideoCall = () => {
   const script = document.createElement("script");
   script.src = "vonage.js";
   script.async = true;
   document.body.appendChild(script);
+
   return (
     <div style={{ height: "100%" }}>
       <div id="videos">
         <div id="subscriber"></div>
         <div id="publisher"></div>
       </div>
+
+      <Link to="/" className="ui button">
+        Exit
+      </Link>
     </div>
   );
 };
