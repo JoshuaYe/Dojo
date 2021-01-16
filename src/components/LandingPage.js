07b215cd818bdfd99ad9e13f20fcf6495fe4aa10
David Liu <d282liu@uwaterloo.ca>
2021-01-16 15:48:48 -0500

    Created index.css style sheet, styled landing page by centering text and changing font

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './index.css';
 
 const LandingPage = () => {
   return (
     <div>
       <h1>DOJO</h1>
       <h2>Teach what you know. Learn what you don't.</h2>
-      <Link to={"/join"} className="ui button primary">
+    <div id = "button">
+    <Link to={"/join"} className="ui button primary">
         Step into the Dojo
       </Link>
+    </div>
     </div>
   );
 };
