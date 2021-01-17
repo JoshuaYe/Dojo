b01b7cfbe0f1be33946f8acebe2184012670d063
JoshuaYe <joshua.ye@live.com>
2021-01-17 01:11:57 -0500

    added logo to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div
       className="ui secondary pointing menu"
       style={{ backgroundColor: "#5c4033" }}
-    ></div>
+    >
+      <a href="/">
+        <img src="/ying-yang.png" alt="logo" width="100" height="auto" />
+      </a>
+    </div>
   );
 };
 
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,7 +5,7 @@ import "./index.css";
 const LandingPage = () => {
   return (
     <div>
-      <h1>DOJO</h1>
+      <h1 id="title">DOJO</h1>
       <h2 id="headline">Teach what you know. Learn what you don't.</h2>
       <div id="button">
         <Link to="/join" className="ui brown button">
