f9c06cc24a999283317f121bab416f51b8db509a
JoshuaYe <joshua.ye@live.com>
2021-01-17 08:18:06 -0500

    fixed navbar elements ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./index.css";
 
 const Header = () => {
   return (
-    <div id = "header"
+    <div
+      id="header"
       className="ui secondary pointing menu"
       style={{ backgroundColor: "#5c4033" }}
     >
       <a href="/">
         <img src="/ying-yang.png" alt="logo" width="100" height="auto" />
+        <div style={{ float: "right" }}>
+          <h1 id="nav">Dojo</h1>
+        </div>
       </a>
     </div>
   );
